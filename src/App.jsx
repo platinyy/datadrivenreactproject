d71518f4d856d5f4cf9b65a5b6a64879d26379e5
Platin Syla <platinsyla06@gmail.com>
2023-07-04 13:38:21 -0400

    Made main component and styled it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
 import Navbar from './componenets/NavBar'
+import Main from './componenets/Main'
+import Card from './componenets/card'
 function App() {
   
 
   return (
    <div>
     <Navbar />
+    <Main />
    </div>
   )
 }
---
+++ src/componenets/Main.jsx
@@ -0,0 +1,12 @@
+function Main (){
+    return (
+        <section className="main">
+            <img src="src/Group 77.png" className="main--photo"/>
+            <h1 className="main--header">Online Experinces</h1>
+            <p className="main--text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}
+
+export default Main

5b030d90e15a828a11963e40a39d07f46bf9a3ae
Platin Syla <platinsyla06@gmail.com>
2023-07-04 13:23:11 -0400

    Made nav component and styled it

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+import Navbar from './componenets/NavBar'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-   
-    </>
+   <div>
+    <Navbar />
+   </div>
   )
 }
 
---
+++ src/componenets/NavBar.jsx
@@ -0,0 +1,11 @@
+
+function Navbar (){
+    return (
+        <nav>
+            
+            <img src="src/airbnb.png" className="nav--logo" />
+        </nav>
+    )
+}
+
+export default Navbar
